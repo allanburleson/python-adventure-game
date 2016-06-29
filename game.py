7ecbe84367ea4cf7af9c2d4c8bb35a046b1a6e0b
disorientedperson <disorientedperson@icloud.com>
2016-06-29 15:08:39 -0500

    Nicer exit with KeyboardInterrupt

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 import readline
 import random
 import shelve
+import sys
 
 import parser
 import locations
@@ -10,23 +11,27 @@ import classes
 player = classes.Player(locations, locations.start)
 previousNoun = ''
 while True:
-    command = parser.parseCommand(input('> '))
-    if command is not None:
-        hasNoun = True
-        action = command[0]
-        if len(command) >= 2:
-            noun = command[1]
-        else:
-            hasNoun = False
-            noun = None
-        if previousNoun != '' and noun == 'it':
-            noun = previousNoun
-        try:
-            commandResult = getattr(player, action)(action, noun, 
-                                                    hasNoun)
-        except AttributeError:
-            print('You can\'t do that here.')
-        if noun is not None:
-            previousNoun = noun
-        else:
-            previousNoun = ''
+    try:
+        command = parser.parseCommand(input('> '))
+        if command is not None:
+            hasNoun = True
+            action = command[0]
+            if len(command) >= 2:
+                noun = command[1]
+            else:
+                hasNoun = False
+                noun = None
+            if previousNoun != '' and noun == 'it':
+                noun = previousNoun
+            try:
+                commandResult = getattr(player, action)(action, noun, 
+                                                        hasNoun)
+            except AttributeError:
+                print('You can\'t do that here.')
+            if noun is not None:
+                previousNoun = noun
+            else:
+                previousNoun = ''
+    except KeyboardInterrupt:
+        print('Bye!')
+        sys.exit(0)
