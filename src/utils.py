10a9c45a1b5ae911b99f00640fabc139b1d18cdf
disorientedperson <disorientedperson@icloud.com>
2016-06-30 13:17:08 -0500

    add getIndefArticle function

--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,10 @@ def getItemFromName(itemName, itemList, player):
         if itemName == item.name:
             return item
     return False
+
+def getIndefArticle(noun):
+    if noun[0] in 'a e i o u'.split(' '):
+        return 'an'
+    elif noun[0] in 'b c d f g h j k l m n p q r s t v w x y z 1 2 3 4'\
+                    '5 6 7 8 9 0'.split(' '):
+        return 'a'
