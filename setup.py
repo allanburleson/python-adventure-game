0907e4f32e4e0bb48f4f101b520ce8f28c731d6c
Allan Burleson <burleson.a@icloud.com>
2017-02-26 21:35:49 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+# TODO: change to rst
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pag',
+    version='0.1.0',
+    description='A simple text-based adventure game engine.',
+    long_description=long_description,
+    url='https://github.com/allanburleson/python-adventure-game',
+    author='Allan Burleson',
+    author_email='burleson.a@icloud.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Education :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='game text text-based adventure interactive-fiction',
+    packages=find_packages(exclude='tests'),
+    package_data={
+        'pag': ['dictionary/*.txt']
+    }
+)
